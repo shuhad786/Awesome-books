6a61d6ddf9ca831b776f255e7cec6bd6d408b4a7
gandradep <eventsonido@gmail.com>
2022-09-19 16:23:48 +0100

    Add function and display localStorage

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-let test = new Object();
+
 const form = document.getElementById('form');
 const title = document.getElementById('title');
 const author = document.getElementById('author');
+let cookie ;
+let cookieArray;
+const bookInfo = document.getElementById('bookInfo');
 let bookArray = [];
+
+if(localStorage.getItem('bookStored') !==null){
+   cookie = localStorage.getItem('bookStored') ;
+  cookieArray = JSON.parse(cookie);
+
+  if (cookieArray) {
+    bookArray=cookieArray;
+  }
+}
 
 
 
@@ -11,6 +23,27 @@ form.addEventListener('submit', function(event){
   let test = new Object();
   test.title = title.value;
   test.author = author.value;
-  bookArray.push(test);
+  let stringObj = JSON.stringify(test);
+  bookArray.push(stringObj);
+  let stringArray = JSON.stringify(bookArray);
+  localStorage.setItem('bookStored', stringArray);
+  bookInfo.innerText = "";
+  displayBooks();
+  // location.reload();
 
 });
+
+function displayBooks (){
+  for (var i = 0; i < bookArray.length; i++) {
+    const titleHtml = document.createElement('p');
+    const authorHtml = document.createElement('p');
+    const remove = document.createElement('button');
+    const recoveredObject=JSON.parse(bookArray[i]);
+    titleHtml.innerText = recoveredObject.title;
+    authorHtml.innerText = recoveredObject.author;
+    remove.innerText = 'Remove!';
+    bookInfo.append(titleHtml, authorHtml, remove);
+
+  }
+}
+displayBooks();
