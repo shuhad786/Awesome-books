2453b89402cf319a09bf783416d02c28153e4146
Shuhad786 <shuhad.loofer@gmail.com>
2022-09-19 14:59:23 +0200

    Created index.html and app.js files

---
+++ app.js
@@ -0,0 +1,21 @@
+const form = document.getElementById('form');
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+let counter = 0;
+
+function atBook(title, author) {
+  this.title = title;
+  this.author = author;
+}
+
+const library = []
+
+if (library == null) {
+  Set
+};
+
+form.addEventListener('submit', () => {
+  let string = `"counter" ${this.author} + ${this.title}`
+  counter = counter + 1;
+  console.log(string);
+});
