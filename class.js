b6f8c18823dc8dde340f560c8b44385c0969ec0d
Shuhad786 <shuhad.loofer@gmail.com>
2022-09-20 16:00:15 +0200

    Updated class.js with display functions

--- class.js
+++ class.js
@@ -1,7 +1,7 @@
 const form = document.getElementById('form');
 const inputTitle = document.getElementById('title');
 const inputAuthor = document.getElementById('author');
-
+const displayList = document.getElementById('bookinfo'); 
 class BookTemplate {
   constructor(title, author) {
     this.title = title;
@@ -15,12 +15,24 @@ class BookList {
   addBook(title, author) {
     let bookAdded = new BookTemplate(title, author);
     this.bookArray.push(bookAdded);
+    localStorage.setItem('BookLibrary', JSON.stringify(this.bookArray));
+  }
+  displayBookList() {
+    bookInfo.innerText = '';
+    this.bookArray.forEach((item) => {
+      const titleHtml = document.createElement('p');
+      const authorHtml = document.createElement('p');
+      const remove = document.createElement('button');
+      titleHtml.innerText = item.title;
+      authorHtml.innerText = item.author;
+      remove.innerText = 'Remove!';
+      bookInfo.append(titleHtml, authorHtml, remove);
+    });
   }
 }
 let myBookList = new BookList();
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  myBookList.addBook(inputTitle.value, inputAuthor.value);
-
-  
+  myBookList.addBook(inputTitle.value, inputAuthor.value); 
+  myBookList.displayBookList();
 });
