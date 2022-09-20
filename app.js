17c3fa0417c82531071b1bfa2583beabb6911ce9
gandradep <eventsonido@gmail.com>
2022-09-20 09:57:54 +0100

    Linters checked.

--- app.js
+++ app.js
@@ -6,42 +6,28 @@ let cookieArray;
 const bookInfo = document.getElementById('bookInfo');
 let bookArray = [];
 
-if(localStorage.getItem('bookStored') !==null){
-   cookie = localStorage.getItem('bookStored') ;
+if (localStorage.getItem('bookStored') !== null) {
+  cookie = localStorage.getItem('bookStored');
   cookieArray = JSON.parse(cookie);
 
   if (cookieArray) {
-    bookArray=cookieArray;
+    bookArray = cookieArray;
   }
 }
-
-form.addEventListener('submit', function(event){
-  event.preventDefault();
-  let test = new Object();
-  test.title = title.value;
-  test.author = author.value;
-  let stringObj = JSON.stringify(test);
-  bookArray.push(stringObj);
-  let stringArray = JSON.stringify(bookArray);
-  localStorage.setItem('bookStored', stringArray);
-  bookInfo.innerText = "";
-  displayBooks();
-});
-
-function displayBooks (){
-  for (var i = 0; i < bookArray.length; i++) {
-    const newNode = document.createTextNode("");
+function displayBooks() {
+  bookArray.forEach((item) => {
+    const newNode = document.createTextNode('');
     const titleHtml = document.createElement('p');
     const authorHtml = document.createElement('p');
     const remove = document.createElement('button');
-    const indexVal = bookArray[i];
-    const recoveredObject=JSON.parse(indexVal);
+    const indexVal = bookArray.indexOf(item);
+    const recoveredObject = JSON.parse(item);
     titleHtml.innerText = recoveredObject.title;
     authorHtml.innerText = recoveredObject.author;
     remove.innerText = 'Remove!';
     bookInfo.append(titleHtml, authorHtml, remove);
-    
-    remove.addEventListener('click', (indexVal) => {
+
+    remove.addEventListener('click', () => {
       bookArray = JSON.parse(localStorage.getItem('bookStored'));
       bookArray.splice(indexVal, 1);
       localStorage.setItem('bookStored', JSON.stringify(bookArray));
@@ -49,6 +35,20 @@ function displayBooks (){
       authorHtml.replaceWith(newNode);
       remove.remove();
     });
-  }
+  });
 }
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const test = {};
+  test.title = title.value;
+  test.author = author.value;
+  const stringObj = JSON.stringify(test);
+  bookArray.push(stringObj);
+  const stringArray = JSON.stringify(bookArray);
+  localStorage.setItem('bookStored', stringArray);
+  bookInfo.innerText = '';
+  displayBooks();
+});
+
 displayBooks();
