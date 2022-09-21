c736d22c8270cb745151358a62953b39f726509a
gandradep <eventsonido@gmail.com>
2022-09-21 14:53:38 +0100

    Fixed linters

--- DateTime.js
+++ DateTime.js
@@ -1,28 +1,28 @@
-let today = new Date();
-let dd = today.getDate();
-let mm = today.getMonth();
-let yyyy = today.getFullYear();
+const today = new Date();
+const dd = today.getDate();
+const mm = today.getMonth();
+const yyyy = today.getFullYear();
 const months = [
-  'January'
-, 'February'
-, 'March'
-, 'April'
-, 'May'
-, 'June'
-, 'July'
-, 'August'
-, 'September'
-, 'October'
-, 'November'
-, 'December'
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
 ];
 const date = document.getElementById('dateTime');
 
-let timeAMPM = today.toLocaleTimeString('en-US', {
+const timeAMPM = today.toLocaleTimeString('en-US', {
   hour: 'numeric',
   minute: 'numeric',
   second: 'numeric',
   hour12: true
-})
+});
 
-date.innerText = months[mm] + ' ' + dd + 'th ' + yyyy + ', ' + timeAMPM;
+date.innerText = `${months[mm]} ${dd}th ${yyyy}, ${timeAMPM}`;
